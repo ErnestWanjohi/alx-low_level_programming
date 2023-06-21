70f2fa6b285b3b4866775a13bb6ecaf77c5b5990
ErnestWanjohi <ernestwanjohi35@gmail.com>
2023-06-20 21:56:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @n1: The first number to be computed
+ * @n2: The send number to be computed
+ * Return: Integer valeu
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
