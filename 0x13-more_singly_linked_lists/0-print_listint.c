8b9f01a2c86b36b3cb5135f424bd65c84ccea178
ErnestWanjohi <ernestwanjohi35@gmail.com>
2023-08-01 04:37:04 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t numb = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		num++;
+		numb++;
 		h = h->next;
 	}
-	return (num);
+	return (numb);
 }
