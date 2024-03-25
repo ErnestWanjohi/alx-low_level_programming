5c925183f0c9ecb736d95daabd688eab73abb768
ErnestWanjohi <ernestwanjohi35@gmail.com>
2024-03-25 16:08:47 +0000

    100-operations.so

---
+++ 0x18-dynamic_libraries/mult.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+int mult (int a, int b){
+return (a*b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * sub - subtracts two integer
+ * @a- first integer
+ * @b - second integer
+ * Return - subtraction
+*/
+int sub(int a, int b){
+return (a-b);
+}
