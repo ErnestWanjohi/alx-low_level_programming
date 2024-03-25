2578e81d0f95f4b4cc75d1fb6b21b9be98ab4726
ErnestWanjohi <ernestwanjohi35@gmail.com>
2024-03-25 17:37:01 +0000

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/ErnestWanjohi/alx-low_level_programming/main/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -P .. https://raw.githubusercontent.com/ErnestWanjohi/alx-low_level_programming/main/0x18-dynamic_libraries/ls.so
+export LD_PRELOAD="$PWD/../ls.so"
